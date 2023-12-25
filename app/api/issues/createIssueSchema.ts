fb5d4224da16eff5332a106f49f561802eeb5fb4
mpcodingpl <97036774+mpcodingpl@users.noreply.github.com>
2023-12-25 14:51:42 +0100

    Handled errors

--- app/api/issues/createIssueSchema.ts
+++ app/api/issues/createIssueSchema.ts
@@ -1,8 +1,8 @@
 import { z } from 'zod'
 
 const createIssueSchema = z.object({
-  title: z.string().min(1),
-  description: z.string().min(1),
+  title: z.string().min(1, 'Title is required'),
+  description: z.string().min(1, 'Description is required'),
 })
 
 export default createIssueSchema
