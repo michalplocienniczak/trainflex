4ec3c6174d11bef963365c4b779136930d3cada0
mpcodingpl <97036774+mpcodingpl@users.noreply.github.com>
2024-02-18 06:08:35 +0100

    Changed the way we make requests in order to reload data on every request

--- hooks/Activites/useGetTrainingActivities.ts
+++ hooks/Activites/useGetTrainingActivities.ts
@@ -1,6 +1,7 @@
 'use client'
 import { Activity, User } from '@prisma/client'
 import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
 
 type UseGetTrainingActivitiesProps = {
   trainingId: string
@@ -12,10 +13,10 @@ export const useGetTrainingActivities = (
   return useQuery({
     queryKey: ['activities', props.trainingId],
     queryFn: async () => {
-      const response = await fetch(
+      const response = await axios.get(
         `/api/trainings/${props.trainingId}/activities`
       )
-      const data: (Activity & { user: User })[] = await response.json()
+      const data: (Activity & { user: User })[] = await response.data
       return data
     },
     enabled: !!props.trainingId,
